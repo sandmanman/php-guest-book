d60a7f7c732cfed211a3627bea6113fc41b86e06
sandman <sandmanman@qq.com>
2016-08-03 14:37:39 +0800

    common header

---
+++ common/header.php
@@ -0,0 +1,11 @@
+<header>
+    <nav class="navbar navbar-default navbar-static-top m-b-0">
+        <div class="navbar-header">
+            <ul class="nav navbar-top-links navbar-right pull-right">
+                <li>
+                    <a href="admin/admin.php">管理</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
