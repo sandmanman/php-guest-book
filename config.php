72c069a722a6bb3686d596cb36715faaeb8b4f73
sandman <sandmanman@qq.com>
2016-08-24 13:35:13 +0800

    config

--- config.php
+++ config.php
@@ -9,8 +9,9 @@
     define( 'DB_PWD', 'root' ); // 数据库密码
     define( 'DB_NAME', 'guestbook' ); // 数据库名
 
-    define( 'GB_TABLE_NAME', 'gb_guestbook' ); // 留言表 表名
-    define( 'ADMIN_TABLE_NAME', 'gb_user' ); // 用户表 表名
+    define( 'GB_TABLE_NAME', 'gb_guestbook' ); // 留言表
+    define( 'REPLY_TABLE_NAME', 'gb_reply' ); // 回复表
+    define( 'ADMIN_TABLE_NAME', 'gb_user' ); // 用户表
 
     date_default_timezone_set('Asia/Shanghai');
 
