9205badc6d00b3712503e171dc1e0634bc8fc0d3
sandman <sandmanman@qq.com>
2016-08-24 15:34:50 +0800

    reply

--- admin/reply.php
+++ admin/reply.php
@@ -9,20 +9,27 @@
   	$content = $_GET['content'];
   	$content = trim($content);
    	$content = htmlspecialchars($content);
-   	$create_time = time();
+   	$reply_time = time();
 
     if ( !empty($content && $content) && $cid!=='undefined' && $content!=='undefined' ) {
     	
-    	$sql = "INSERT INTO ".REPLY_TABLE_NAME."(cid,content,create_time) VALUES('$cid', '$content', '$create_time')";
-	    $result = $dbhelper -> execute_dql($sql);
+        $sql = "INSERT INTO ".REPLY_TABLE_NAME."(cid,content,reply_time) VALUES('$cid', '$content', '$reply_time')";
+        $result = $dbhelper -> execute_dql($sql);
 
-	    if ( $result ) {
-	    	echo 1; // 回复成功
-	    } else {
-	    	echo 0; // 回复失败
-	    }
+  	    if ( $result ) {
+            // 改变留言表对应cid的status
+            $sql_status = "UPDATE ".GB_TABLE_NAME." SET status='1' WHERE cid='$cid'";
+            $res_status = $dbhelper -> execute_dql($sql_status);
 
-	    $dbhelper -> close_dbc();
+            if ($res_status) {
+              echo 1; // 回复成功
+            }
+  	    	
+  	    } else {
+  	    	  echo 0; // 回复失败
+  	    }
+
+  	    $dbhelper -> close_dbc();
 
     } else {
     	echo '参数错误';
